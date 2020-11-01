8b6b6a608a670a973e54957dfd14a7c48898621d
Hari <sharipsg@gmail.com>
2020-11-01 12:57:46 +0530

    init spring boot app

---
+++ src/test/java/com/squareshift/quartz_scheduler/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.squareshift.quartz_scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
